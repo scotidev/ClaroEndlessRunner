853ec3009e7cd000620a71547e67d0483c61dd73
scotidev <scotidev@gmail.com>
2025-12-04 07:55:55 -0300

    Platform movement

--- Assets/Environment/Scripts/SpawnPlatform.cs
+++ Assets/Environment/Scripts/SpawnPlatform.cs
@@ -4,32 +4,47 @@ using UnityEngine;
 public class SpawnPlatform : MonoBehaviour
 {
     public List<GameObject> platforms = new List<GameObject>();
+    public List<Transform> currentPlatforms = new List<Transform>();
     public int offset;
-
-    public GameObject platformPrefab;
+    private Transform player;
+    private Transform currentPlatformPoint;
+    private int platformIndex;
 
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
         for (int i = 0; i < platforms.Count; i++)
         {
-            Instantiate(platforms[i], new Vector3(0, 0, i * 31), transform.rotation);
-            offset += 31;
+            Transform p = Instantiate(platforms[i], new Vector3(0, 0, i * 30), transform.rotation).transform;
+            currentPlatforms.Add(p);
+            offset += 30;
         }
+
+        currentPlatformPoint = currentPlatforms[platformIndex].GetComponent<Platform>().point;
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R))
+        float distance = player.position.z - currentPlatformPoint.position.z;
+
+        if (distance >= 5)
         {
-            Debug.Log("Pressionei R");
-            Recycle(platformPrefab);
-            Debug.Log(platformPrefab);
+            Recycle(currentPlatforms[platformIndex].gameObject);
+            platformIndex++;
+
+            if (platformIndex > currentPlatforms.Count - 1)
+            {
+                platformIndex = 0;
+            }
+
+            currentPlatformPoint = currentPlatformPoint = currentPlatforms[platformIndex].GetComponent<Platform>().point;
         }
     }
 
     public void Recycle(GameObject platform)
     {
         platform.transform.position = new Vector3(0, 0, offset);
-        offset += 31;
+        offset += 30;
     }
 }
